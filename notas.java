d2a2a2c8b4a1396cb475d28ef8f80e0805c27bc3
alejandro <ajreverolr@ufpso.edu.co>
2025-08-21 21:08:36 -0500

    primera semana

--- notas.java
+++
@@ -1,6 +0,0 @@
-public class notas{
-public static void main(String[] args) {
-    
-System.out.println("hello wordl");
-}
-}
---
+++ notas/notas.java
@@ -0,0 +1,23 @@
+package notas;
+public class notas{
+public static class BankAccount {
+String accountNumber;
+String owner;
+double balance ;
+
+    
+} {
+class app {
+public static void main(String[] args) throws Exception {
+    BancoAccount account1 = new BancoAccount();
+    account1.owner= "alejandro";
+    System.out.println(account1.owner);
+
+}
+
+
+    
+}
+    
+}
+}
